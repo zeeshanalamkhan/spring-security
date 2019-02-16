9e93e320ff8fbbe17934b6cdbbfd5b51e86d4366
ZEESHAN KHAN <zeeshan.maths@gmail.com>
2019-02-17 00:38:18 +0530

    sring boot application main(-) class

---
+++ spring-boot-security-custom-login-page/src/main/java/com/zeeshan/SpringBootSecurityCustomLoginPageApplication.java
@@ -0,0 +1,14 @@
+package com.zeeshan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecurityCustomLoginPageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSecurityCustomLoginPageApplication.class, args);
+	}
+
+}
+
